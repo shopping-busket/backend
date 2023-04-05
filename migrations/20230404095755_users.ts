f07f026014ad2649eb110ccd3f2251a83b9993c0
ilja <ilja.ragnar@gmail.com>
2023-04-05 17:56:42 +0200

    corrected migrations to work for new dbs

--- migrations/20230404095755_users.ts
+++ migrations/20230404095755_users.ts
@@ -1,16 +1,28 @@
-import { Knex } from "knex";
+import { Knex } from 'knex';
 
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('users', (table) => {
-    table.unique(['uuid']);
+    table.increments('id');
+    table.uuid('uuid').unique();
+
+    table.string('email');
+    table.string('password');
+
+    table.string('fullName');
+    table.string('avatarURI');
+
+    table.string('preferredLanguage');
+    table.boolean('prefersDarkMode');
+    table.boolean('prefersMiniDrawer');
+
+    table.string('googleId');
+    table.string('githubId');
   });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('users', (table) => {
-    table.dropUnique(['uuid']);
-  });
+  await knex.schema.dropTable('users');
 }
 
---
+++ migrations/20230405155101_list.ts
@@ -0,0 +1,25 @@
+import { Knex } from 'knex';
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('list', (table) => {
+    table.increments('id');
+
+    table.uuid('listid').notNullable();
+    table.uuid('owner').notNullable();
+
+    table.string('name').notNullable();
+    table.string('description');
+
+    table.specificType('entries', 'JSON[]');
+    table.specificType('checkedEntries', 'JSON[]');
+
+    table.string('backgroundURI').nullable();
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('list');
+}
+
