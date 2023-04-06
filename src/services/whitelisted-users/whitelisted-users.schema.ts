9308e590af48d2f20b4634c61df0de058fa65a5d
ilja <ilja.ragnar@gmail.com>
2023-04-06 23:52:30 +0200

    Secured whitelisted-users.schema.ts service

--- src/services/whitelisted-users/whitelisted-users.schema.ts
+++ src/services/whitelisted-users/whitelisted-users.schema.ts
@@ -7,6 +7,8 @@ import type { HookContext } from '../../declarations';
 import { dataValidator, queryValidator } from '../../validators';
 import { app } from '../../app';
 import { BadRequest } from '@feathersjs/errors';
+import { WhitelistedUsersParams } from './whitelisted-users.class';
+import { List } from '../list/list.schema';
 
 // Main data model schema
 export const whitelistedUsersSchema = Type.Object(
@@ -15,6 +17,7 @@ export const whitelistedUsersSchema = Type.Object(
     user: Type.Optional(Type.String({ format: 'uuid' })),
     inviteEmail: Type.Optional(Type.String({ format: 'email' })),
     listId: Type.String({ format: 'uuid' }),
+    inviteSecret: Type.String({ format: 'uuid' }),
 
     canEditEntries: Type.Optional(Type.Boolean({ default: true })),
     canDeleteEntries: Type.Optional(Type.Boolean({ default: false })),
@@ -22,10 +25,11 @@ export const whitelistedUsersSchema = Type.Object(
   { $id: 'WhitelistedUsers', additionalProperties: false },
 );
 export type WhitelistedUsers = Static<typeof whitelistedUsersSchema>;
-export const whitelistedUsersValidator = getValidator(whitelistedUsersSchema, dataValidator);
 export const whitelistedUsersResolver = resolve<WhitelistedUsers, HookContext>({});
 
-export const whitelistedUsersExternalResolver = resolve<WhitelistedUsers, HookContext>({});
+export const whitelistedUsersExternalResolver = resolve<WhitelistedUsers, HookContext>({
+  inviteSecret: async () => undefined,
+});
 
 // Schema for creating new entries
 export const whitelistedUsersDataSchema = Type.Pick(whitelistedUsersSchema, ['inviteEmail', 'listId'], {
@@ -53,12 +57,39 @@ export const whitelistedUsersDataResolver = resolve<WhitelistedUsers, HookContex
 });
 
 // Schema for updating existing entries
-export const whitelistedUsersPatchSchema = Type.Partial(whitelistedUsersSchema, {
+export const whitelistedUsersPatchSchema = Type.Pick(whitelistedUsersSchema, ['user', 'canDeleteEntries', 'canEditEntries'] as (keyof WhitelistedUsers)[], {
   $id: 'WhitelistedUsersPatch',
 });
 export type WhitelistedUsersPatch = Static<typeof whitelistedUsersPatchSchema>;
 export const whitelistedUsersPatchValidator = getValidator(whitelistedUsersPatchSchema, dataValidator);
-export const whitelistedUsersPatchResolver = resolve<WhitelistedUsers, HookContext>({});
+export const whitelistedUsersPatchResolver = resolve<WhitelistedUsers, HookContext>({
+  user: async (value, whitelist, ctx) => {
+    if ((ctx.params as WhitelistedUsersParams).user?.uuid === value || value == null) return value;
+    throw new BadRequest('Only the user this whitelist entry belongs to (not the list\'s creator) can edit the user column!');
+  },
+  inviteSecret: async (value, whitelist, ctx) => {
+    const id = ctx.arguments[0];
+    if (id == null) throw new BadRequest('whitelist.id not allowed to be null at this point: whitelistedUsersPatchResolver.inviteSecret()');
+
+    const knex = app.get('postgresqlClient');
+
+    const whitelistedUser = await knex('whitelisted-users').select().where({
+      id: id,
+    }).first() as WhitelistedUsers;
+    const { owner: listOwner } = await knex('list').select('owner').where({
+      listid: whitelistedUser.listId,
+    }).first() as Pick<List, 'owner'>;
+
+    let allow = true;
+    Object.keys(ctx.arguments[1] as WhitelistedUsers).forEach((key) => {
+      if (key !== 'canEditEntries' && key !== 'canDeleteEntries') allow = false;
+      else if (listOwner !== (ctx.params as WhitelistedUsersParams).user?.uuid) allow = false;
+    });
+
+    if (allow || (whitelistedUser.user != null || value != null)) return undefined;
+    throw new BadRequest('inviteSecret shouldn\'t be null or undefined until user uuid is set!');
+  },
+});
 
 // Schema for allowed query properties
 export const whitelistedUsersQueryProperties = Type.Pick(whitelistedUsersSchema, ['id', 'user', 'listId']);
