55e7b995b501847cd10caf965314421b33901d5c
ilja <ilja.ragnar@gmail.com>
2023-04-07 19:47:54 +0200

    allowed whitelisted users to access and change list data, moved to external resolver so others also cannot patch, update or remove

--- src/services/list/list.schema.ts
+++ src/services/list/list.schema.ts
@@ -8,6 +8,7 @@ import { dataValidator, queryValidator } from '../../validators';
 import { User } from '../users/users.schema';
 import { Forbidden } from '@feathersjs/errors';
 import { app } from '../../app';
+import { WhitelistedUsers } from '../whitelisted-users/whitelisted-users.schema';
 
 
 const entryProperties = {
@@ -36,7 +37,33 @@ export type List = Static<typeof listSchema>
 export const listValidator = getValidator(listSchema, dataValidator);
 export const listResolver = resolve<List, HookContext>({});
 
-export const listExternalResolver = resolve<List, HookContext>({});
+export const listExternalResolver = resolve<List, HookContext>({
+  id: async (value, shoppingList, context) => {
+    if (context.method === 'create') return value;
+
+    const knex = app.get('postgresqlClient');
+    const userUUID = (context.params.user as User).uuid;
+    if (!userUUID) return;
+
+    if (shoppingList.owner && shoppingList.owner === userUUID) return value;
+    else if (shoppingList.listid) {
+      const whitelist = await knex('whitelisted-users').select('user', 'listId').where({
+        listId: shoppingList.listid,
+      }) as Pick<WhitelistedUsers, 'user' | 'listId'>[];
+
+      let isWhitelisted = false;
+      whitelist.forEach((whitelist) => {
+        if (!isWhitelisted && whitelist.user === userUUID) isWhitelisted = true;
+      });
+
+      const { owner } = (await knex('list').select('owner').where({ listid: shoppingList.listid }).first() as {
+        owner: string
+      } | null) ?? { owner: null };
+      if (isWhitelisted || (owner != null && owner === userUUID)) return value;
+    }
+    throw new Forbidden('You are not allowed to access this content.');
+  },
+});
 
 // Schema for creating new entries
 export const listDataSchema = Type.Pick(listSchema, ['listid', 'owner', 'name', 'description', 'entries', 'checkedEntries', 'backgroundURI'], {
@@ -66,27 +93,4 @@ export const listQuerySchema = Type.Intersect(
 );
 export type ListQuery = Static<typeof listQuerySchema>
 export const listQueryValidator = getValidator(listQuerySchema, queryValidator);
-export const listQueryResolver = resolve<ListQuery, HookContext>({
-  id: async (value, shoppingList, context) => {
-    if (context.method === 'create') return value;
-
-    const knex = app.get('postgresqlClient');
-    const userUUID = (context.params.user as User).uuid;
-    if (!userUUID) return;
-
-    if (shoppingList.owner && shoppingList.owner === userUUID) return value;
-    else if (shoppingList.listid) {
-      // let isAllowed = false;
-
-      // Allow users who joined a shared list to access the lists data
-      // const shared = await knex('share-link').select().where({ pointsTo: shoppingList.listid }) as ShareLink[];
-      // shared.forEach(share => {
-        // if (share.users.includes(userUUID)) isAllowed = true;
-      // });
-
-      const { owner } = (await knex('list').select('owner').where({ listid: shoppingList.listid }).first() as { owner: string } | null) ?? { owner: null };
-      if (/*isAllowed || */(owner != null && owner === userUUID)) return value;
-    }
-    throw new Forbidden('You are not allowed to access this content.');
-  },
-});
+export const listQueryResolver = resolve<ListQuery, HookContext>({});
