2141fdbf91c837f01e38eecba10439fea297bbbc
justmedev <ilja.ragnar@gmail.com>
2023-04-12 09:32:06 +0200

    removed global app.publish

--- src/channels.ts
+++ src/channels.ts
@@ -3,13 +3,8 @@ import type { Params, RealTimeConnection } from '@feathersjs/feathers';
 import type { AuthenticationResult } from '@feathersjs/authentication';
 import '@feathersjs/transport-commons';
 import type { Application } from './declarations';
-import { logger } from './logger';
 
 export const channels = (app: Application) => {
-  logger.warn(
-    'Publishing all events to all authenticated users. See `channels.ts` and https://dove.feathersjs.com/api/channels.html for more information.',
-  );
-
   app.on('connection', (connection: RealTimeConnection) => {
     // On a new real-time connection, add it to the anonymous channel
     app.channel('anonymous').join(connection);
@@ -25,14 +20,5 @@ export const channels = (app: Application) => {
       // Add it to the authenticated user channel
       app.channel('authenticated').join(connection);
     }
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  app.publish(() => {
-    // Here you can add event publishers to channels set up in `channels.js`
-    // To publish only for a specific event use `app.publish(eventname, () => {})`
-
-    // e.g. to publish all service events to all authenticated users use
-    return app.channel('authenticated');
   });
 };
