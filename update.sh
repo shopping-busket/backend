6731ab21848ad02c46e81031d91af18c9a637ed2
JustMe <ilja.ragnar@gmail.com>
2023-04-19 06:34:57 +0200

    add warn to not run this as root, compile backend, add colorized output

--- update.sh
+++ update.sh
@@ -5,16 +5,27 @@
 # backend/
 #  ↳ update.sh
 # web/
+
+COLOR_CYAN="\u001b[38;5;4m"
+COLOR_WARN="\u001b[38;5;220m"
+COLOR_RESET="\u001b[0m"
+TAG="${COLOR_CYAN}[update.sh]$COLOR_RESET"
+
 cd "${0%/*}" || exit
 cd ..
 
+if [ "$(id -u)" -eq 0 ]; then
+  echo -e "$TAG ${COLOR_WARN}WARNING: This program shall not be run with root. Exiting!$COLOR_RESET"
+  exit
+fi
+
 pull() {
-  echo "Pulling $1..."
+  echo -e "$TAG Pulling $1..."
   if cd "$1" || exit && git pull; then
-    echo "git pull $1: OK"
+    echo -e "$TAG git pull $1: OK"
     cd ..
   else
-    echo "git pull $1: Failed!"
+    echo -e "$TAG git pull $1: Failed!"
     exit
   fi
 }
@@ -29,17 +40,27 @@ compile_frontend() {
 }
 
 flush_copy_frontend() {
-  pwd
-  echo "Deleting all files in backend/public"
+  echo -e "$TAG Deleting all files in backend/public"
   rm -rf "backend/public/"*
 
-  echo "Copying compiled frontend to backend's public dir"
+  echo -e "$TAG Copying compiled frontend to backend's public dir"
   cp -r "web/dist/"* "backend/public/"
+}
+
+compile_backend() {
+  echo -e "$TAG Compiling backend..."
+  (
+    cd "backend" || exit
+    yarn compile
+  )
 }
 
 pull "backend"
 pull "web"
 compile_frontend
 flush_copy_frontend
+compile_backend
 
-echo "Done! Start service using 'systemctl start busket'"
+echo -e "$TAG Done! Enter password to start via systemctl"
+sudo systemctl restart busket
+echo -e "$TAG Restartet Busket service. Busket will be back online with the new updates in a few minutes!"
