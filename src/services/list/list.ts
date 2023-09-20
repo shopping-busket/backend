7197dfed025ae00ff8d89316cef5e162ea78036d
justmedev <ilja.ragnar@gmail.com>
2023-09-20 22:25:50 +0200

    null safety, if array is only 1 el, return el as obj and not in array

--- src/services/list/list.ts
+++ src/services/list/list.ts
@@ -66,8 +66,10 @@ export const list = (app: Application) => {
         return ctx;
       }],
       create: [async (ctx: HookContext<ListService>) => {
+        if (ctx.result === undefined) return;
         const data = _.castArray(ctx.result);
         await Promise.all(data.map(d => addToLibrary(requireDataToBeObject(d)?.owner ?? 'Error', requireDataToBeObject(d)?.listid ?? 'Error')));
+        if (data && data.length == 1) ctx.result = _.clone(data[0]);
       }],
     },
     error: {
