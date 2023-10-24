7029bd6953b07996a7bed44c4b91c8cc1ec8bbea
justmedev <ilja.ragnar@gmail.com>
2023-10-24 19:02:23 +0200

    fix markEntryAs often causing race conditions

--- src/services/event/eventReceiver.ts
+++ src/services/event/eventReceiver.ts
@@ -76,8 +76,23 @@ export class EventReceiver {
   }
 
   public async markEntryAs(data: EventData, markAsDone: boolean): Promise<EventData> {
-    await this.deleteEntry(data, !markAsDone)
-    await this.createEntry(data, markAsDone)
+    await this.postgresClient.raw('BEGIN;');
+    const rows = (await this.postgresClient.raw('select pos - 1 as pos from list, jsonb_array_elements(:col:->\'items\') with ordinality arr(elems, pos) where elems ->> \'id\' = :entryId and "listid" = :listId limit 1 for update;', {
+      col: this.getListByCheckedState(!markAsDone),
+      entryId: data.eventData.entryId,
+      listId: data.listid,
+    })).rows;
+    if (rows.length <= 0) throw new NotFound(`Entry ${data.eventData.entryId} cannot be found!`);
+    await this.postgresClient.raw('update list set :col: = jsonb_set(:col:, \'{items}\', (:col:->\'items\') - :index::int), :toCol: = jsonb_insert(:toCol:, \'{items,:atIndex:}\', :data::jsonb) where "listid" = :listId;', {
+      col: this.getListByCheckedState(!markAsDone),
+      toCol: this.getListByCheckedState(markAsDone),
+      listId: data.listid,
+      entryId: data.eventData.entryId,
+      index: rows[0].pos,
+      atIndex: 0,
+      data: { id: data.eventData.entryId, name: data.eventData.state.name },
+    });
+    await this.postgresClient.raw('COMMIT;');
     return data;
   }
 
